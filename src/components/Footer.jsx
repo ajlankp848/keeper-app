9602a4b9841e0302d02def0fa61ecf90e3d49435
ajlankp848 <ajlantkm848@gmail.com>
2020-07-20 23:04:32 +0530

    react - keeper app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    const currentYear = new Date().getFullYear();
+    return <div>
+        <footer>
+        <p>
+            Copyright © {currentYear} @ajlan-kp
+        </p>
+        </footer>
+        
+    </div>
+}
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1 className="note h1">This is the Heading</h1>,
+        <p className="note p">This is the Paragraph</p>
+    </div>
+}
+export default Note;
