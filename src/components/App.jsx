391b71c865b36f98b523f75be9a4bdd4b79b3c72
ajlankp848 <ajlantkm848@gmail.com>
2020-07-22 12:05:47 +0530

    keeper app updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note"
+import notes from "../notes"
+
 
 
 
 function App(){
     return <div>
         <Header></Header>
-        <Note></Note>
+       {notes.map(function(notesItem){
+    return  <Note
+    title={notesItem.title}
+    content= {notesItem.content}></Note>
+}
+)}
+
+
+
+
+
+
+
         <Footer></Footer>
     </div>
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return <div className="note">
-        <h1 className="note h1">This is the Heading</h1>,
-        <p className="note p">This is the Paragraph</p>
+        <h1 >{props.title}</h1>,
+        <p >{props.content}</p>
     </div>
 }
 export default Note;
